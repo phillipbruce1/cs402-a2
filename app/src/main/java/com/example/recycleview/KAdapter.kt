9bee35d5851e1a00d35d5a773c591e93a94ecdcc
Phillip <phillip.bruce@hey.com>
2021-09-28 19:14:14 -0600

    Added new format for to do list items. Next step: figure out how to edit each item.

--- app/src/main/java/com/example/recycleview/KAdapter.kt
+++ app/src/main/java/com/example/recycleview/KAdapter.kt
@@ -4,33 +4,36 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-public class KAdapter(context: Context, var coffee: ArrayList<String>) :
-    RecyclerView.Adapter<KAdapter.KoffeeHolder>() {
+class ListAdapter(context: Context, private var itemList: ArrayList<String>) :
+    RecyclerView.Adapter<ListAdapter.ListItemHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
-            : KAdapter.KoffeeHolder {
-        val view = LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.coffee_item_view, parent, false)
-        return KoffeeHolder(view)
+            : ListAdapter.ListItemHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_view, parent, false)
+        return ListItemHolder(view)
     }
 
-    override fun getItemCount() = coffee.size
+    override fun getItemCount() = itemList.size
 
     /**
      * Executed when binding the list item to the View
      */
-    override fun onBindViewHolder(holder: KoffeeHolder, position: Int) {
-        val acoffee = coffee[position]
+    override fun onBindViewHolder(holder: ListItemHolder, position: Int) {
+        val item = itemList[position]
         holder.apply {
-            titleTextView.text = acoffee
-        }
+            itemView.findViewById<TextView>(R.id.textView).apply {
+                text = item;
+            };
+        };
     }
 
-    class KoffeeHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = view.findViewById(R.id.coffee_name)
+    class ListItemHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = view.findViewById<RelativeLayout>(R.id.list_item).findViewById<TextView>(R.id.textView);
     }
 }
--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,38 +2,48 @@ package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 // https://cs.boisestate.edu/~scutchin/cs402/codesnips/RecycleView.html
 
-// TODO: refactor project to not be about coffee or koffee
+// DONE: refactor project to not be about coffee or koffee
 
-// TODO: add checkmark buttons to coffee_item_view and add text into checkmark items
+// DONE: add checkmark buttons to coffee_item_view and add text into checkmark items
 // use click listeners
+
+// TODO: edit text?
+// https://androidessence.com/how-to-build-a-todo-list-in-kotlin-part-2-recyclerview
 
 // TODO: plus button in top or bottom right for add
 
-// TODO: on long press, show pop-up dialogue to modify or delete item
+// TODO: on long press, show pop-up dialogue to modify (join) or delete item
+// concat list items with commas
 // https://www.geeksforgeeks.org/how-to-detect-long-press-in-android/
 class MainActivity : AppCompatActivity() {
-    private lateinit var kRecyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView;
+    private val itemList = arrayListOf<String>();
+    // insert adapter here
+    // NOTE: every recycle view requires an adapter
+    private val adapter: ListAdapter = ListAdapter(this, itemList);
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         // instantiate RecyclerView object and associate it with its view
-        kRecyclerView = findViewById<RecyclerView>(R.id.coffee_recycler_view)
-        kRecyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view);
+        recyclerView.layoutManager = LinearLayoutManager(this);
 
-        // instantiate hardcoded arraylist
-        val koffeeList = arrayListOf<String>("Arabic", "Robusta", "Sumatra", "Kona")
+        itemList.add("TODO");
+        adapter.notifyItemInserted(itemList.size - 1);
 
-        // insert adapter here
-        // NOTE: every recycle view requires an adapter
-        val kadapter: KAdapter = KAdapter(this, koffeeList);
+        recyclerView.adapter = adapter;
+    }
 
-        kRecyclerView.adapter = kadapter;
+    fun onAddListItem(view: View) {
+        itemList.add("");
+        adapter.notifyItemInserted(itemList.size - 1);
     }
 }
